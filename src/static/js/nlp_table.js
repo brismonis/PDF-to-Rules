92c43ff028a92e1c7d513a61edb37a40ad4eca0b
Susanne Bair <susannebair@gmx.de>
2021-08-18 14:03:49 +0200

    WORKING: deleting and adding rows to nlp table with JS NOT DONE YET: saving table

---
+++ src/static/js/nlp_table.js
@@ -0,0 +1,22 @@
+function deleteRow(index) {
+    document.getElementById("nlp-table").deleteRow(index);
+    
+}
+
+function addRow() {
+    
+    
+    var tbodyRef = document.getElementById('nlp-table').getElementsByTagName('tbody')[0];
+    var rowindex = tbodyRef.rows.length
+    //document.getElementById("nlp-table").addRow()
+    var newRow = tbodyRef.insertRow(-1);
+    var myHtmlContent = `<tr><td><button onclick="deleteRow(parentNode.parentNode.rowIndex)" type="button" class="btn btn-outline-danger btn-sm py-0 style="font-size: 0.8em;"><i class="fas fa-trash-alt"></i></button></td>
+    <td style="width: 30px;">` + rowindex + `</td>
+    <td><span><textarea name="" id="" style="font-family: Arial;font-size: 12pt; width: 100%; border: none; height: 30px; text-align: center; background: none;"></textarea> </span></td>
+    <td> Evidence </td>
+    <td> <div>
+        <input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="">
+      </div> </td>
+</tr>`
+    newRow.innerHTML = myHtmlContent;
+}
--- src/static/js/spinner_nlp.js
+++ src/static/js/spinner_nlp.js
@@ -1,4 +1,6 @@
-const spin_btn2 = document.getElementById('spin_nlp')
-spin_btn2.onclick = function() {
-    spin_btn2.innerHTML = `Processing NLP ... <div class="spinner-border text-light" role="status"><span class="sr-only"></span></div>`;
+function spin_nlp() {
+    const spin_btn2 = document.getElementById('spin_nlp')
+    spin_btn2.onclick = function() {
+        spin_btn2.innerHTML = `Processing NLP ... <div class="spinner-border text-light" role="status"><span class="sr-only"></span></div>`;
+    }
 }
